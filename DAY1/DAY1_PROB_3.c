71fa92a093ffcb888c3ed8d55e3fdd7056424766
MadhuDadanje <108974027+MadhuDadanje@users.noreply.github.com>
2022-07-11 15:54:19 +0530

    Create DAY1_PROB_3.c

---
+++ DAY1/DAY1_PROB_3.c
@@ -0,0 +1,16 @@
+//The entire area of a circular garden
+
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    float a;
+    printf("enter char.\n");
+    scanf("%d",&r);
+    
+  a=3.14*r*r;
+  
+  printf("area of circle=%f\n",a);
+    return 0;
+}
