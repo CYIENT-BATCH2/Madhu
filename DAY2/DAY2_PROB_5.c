2d344aef4137cedea05fca33abaffdc0c1ee3ae2
MadhuDadanje <108974027+MadhuDadanje@users.noreply.github.com>
2022-07-13 22:44:13 +0530

    Update DAY2_PROB_5.c

--- DAY2/DAY2_PROB_5.c
+++ DAY2/DAY2_PROB_5.c
@@ -1,5 +1,5 @@
 /*.A message is encoded in the form of numbers and sent to you. Write a program to decode the message. (HINT: A=1, B=2, … so on) 
-
+steps:
 1.read n
 2.convert int to alphabet by adding 64 to it 
 3.checking each 2digit no.. at a time 
