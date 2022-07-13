67cdf3b3dfe8d7168025da95a1e0f8ca51d41b1c
MadhuDadanje <108974027+MadhuDadanje@users.noreply.github.com>
2022-07-13 22:30:43 +0530

    Update DAY1_PROB_2.c

--- DAY1/DAY1_PROB_2.c
+++ DAY1/DAY1_PROB_2.c
@@ -1,5 +1,11 @@
-//if it is a vowel or consonant
+/*if it is a vowel or consonant
 
+steps:
+1.read charecter.
+2.check whether the data is alphbet or not
+3.if that charecter is vowel or consonant.
+4.print with checking with that.
+*/
 #include <stdio.h>
 
 int main()
