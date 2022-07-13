9f78c23d45ada7aee8986e219e005aaad0f9774a
MadhuDadanje <108974027+MadhuDadanje@users.noreply.github.com>
2022-07-13 15:49:46 +0530

    Create DAY1_PROB_5.c

---
+++ DAY1/DAY1_PROB_5.c
@@ -0,0 +1,21 @@
+
+
+/*balls are numbered with random 1-digit number each. Assign these numbers using user input. 
+steps:
+1.read n.
+2.checking even or odd by data%2==0
+3.printing it belongs to.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n1;
+  printf("enter no.=")
+    scanf("%d",&n1);
+if(n1%2==0)
+printf("n1 is even");
+else 
+printf("n1 is odd");
+   
+}
