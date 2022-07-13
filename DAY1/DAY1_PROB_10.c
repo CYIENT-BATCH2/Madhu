f25d95de9b8f3666d75492bc2c2eec86478021ff
MadhuDadanje <108974027+MadhuDadanje@users.noreply.github.com>
2022-07-13 22:38:29 +0530

    Update DAY1_PROB_10.c

--- DAY1/DAY1_PROB_10.c
+++ DAY1/DAY1_PROB_10.c
@@ -1,5 +1,13 @@
-//Find the heaviest
+/*Find the heaviest
 
+steps:
+1.read 3 values
+2.checking which is grater
+3. if n1 is greater than both n2 and n3, n1 is the largest.
+4.if n2 is greater than both n1 and n3, n2 is the largest.
+5.if n3 is greater than both n1 and n2, n3 is the largest
+6.prints which is grater.
+*/
 
 #include <stdio.h>
 
